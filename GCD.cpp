d1d2e906adb9d8430e8bfda1172951fdde39831b
Harshini-Maddali <69516405+Harshini-Maddali@users.noreply.github.com>
2021-05-29 15:30:53 +0530

    Add files via upload

---
+++ GCD.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	while(1)
+	{
+		if(a<b)
+		{
+		   swap(a,b);
+	    }
+		(a=a%b);
+		if(a==0)
+		{
+			break;
+		}   
+   }
+   cout<<b;
+   return 0;
+}
